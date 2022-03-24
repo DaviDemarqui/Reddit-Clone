bc1f4be6b0b9c57d5c6424d24e26c7713371b81c
Davi Demarqui <davipersonal@protonmail.com>
2022-03-23 21:24:45 -0300

    RDT-3 Finished the creation of the Repositories + VoteType Enum

---
+++ src/main/java/com/demarqui/reddit/repository/VerificationTokenRepository.java
@@ -0,0 +1,12 @@
+package com.demarqui.reddit.repository;
+
+import com.demarqui.reddit.model.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    Optional<VerificationToken> findByToken(String token);
+}
---
+++ src/main/java/com/demarqui/reddit/repository/VoteRepository.java
@@ -0,0 +1,14 @@
+package com.demarqui.reddit.repository;
+
+import com.demarqui.reddit.model.Post;
+import com.demarqui.reddit.model.User;
+import com.demarqui.reddit.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
+}
